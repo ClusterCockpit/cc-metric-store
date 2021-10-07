85591e7a03236c7f35561077439747442f6de8f9
Lou Knauer <lou.knauer@gmx.de>
2021-10-07 14:52:16 +0200

    Switch to influxes line protocol parser

--- lineprotocol.go
+++ lineprotocol.go
@@ -3,16 +3,13 @@ package main
 import (
 	"bufio"
 	"context"
-	"errors"
-	"io"
 	"log"
 	"math"
 	"net"
 	"strconv"
-	"strings"
 	"sync"
-	"time"
 
+	"github.com/influxdata/line-protocol/v2/lineprotocol"
 	"github.com/nats-io/nats.go"
 )
 
@@ -56,114 +53,15 @@ type Metric struct {
 	Value Float
 }
 
-// measurement: node or cpu
-// tags:        host, cluster, cpu (cpu only if measurement is cpu)
-// fields:      metrics...
-// t:           timestamp (accuracy: seconds)
-type Line struct {
-	Measurement string
-	Tags        map[string]string
-	Fields      []Metric
-	Ts          time.Time
-}
-
-// Parse a single line as string.
-//
-// There is performance to be gained by implementing a parser
-// that directly reads from a bufio.Scanner.
-func ParseLine(rawline string) (*Line, error) {
-	line := &Line{}
-	parts := strings.Fields(rawline)
-	if len(parts) != 3 {
-		return nil, errors.New("line format error")
-	}
-
-	tagsAndMeasurement := strings.Split(parts[0], ",")
-	line.Measurement = tagsAndMeasurement[0]
-	line.Tags = map[string]string{}
-	for i := 1; i < len(tagsAndMeasurement); i++ {
-		pair := strings.Split(tagsAndMeasurement[i], "=")
-		if len(pair) != 2 {
-			return nil, errors.New("line format error")
-		}
-		line.Tags[pair[0]] = pair[1]
-	}
-
-	rawfields := strings.Split(parts[1], ",")
-	line.Fields = []Metric{}
-	for i := 0; i < len(rawfields); i++ {
-		pair := strings.Split(rawfields[i], "=")
-		if len(pair) != 2 {
-			return nil, errors.New("line format error")
-		}
-		field, err := strconv.ParseFloat(pair[1], 64)
-		if err != nil {
-			return nil, err
-		}
-
-		line.Fields = append(line.Fields, Metric{
-			Name:  pair[0],
-			Value: Float(field),
-		})
-	}
-
-	unixTimestamp, err := strconv.ParseInt(parts[2], 10, 64)
-	if err != nil {
-		return nil, err
-	}
-
-	line.Ts = time.Unix(unixTimestamp, 0)
-	return line, nil
-}
-
-func ParseLines(raw string) ([]*Line, error) {
-	lines := make([]*Line, 0, 1)
-	for _, line := range strings.Split(raw, "\n") {
-		if len(line) == 0 {
-			continue
-		}
-
-		line, err := ParseLine(line)
-		if err != nil {
-			return nil, err
-		}
-		lines = append(lines, line)
-	}
-	return lines, nil
-}
-
 // Listen for connections sending metric data in the line protocol format.
 //
 // This is a blocking function, send `true` through the channel argument to shut down the server.
 // `handleLine` will be called from different go routines for different connections.
 //
-func ReceiveTCP(address string, handleLine func(line *Line), done chan bool) error {
+func ReceiveTCP(address string, handleLine func(dec *lineprotocol.Decoder), done chan bool) error {
 	ln, err := net.Listen("tcp", address)
 	if err != nil {
 		return err
-	}
-
-	handleConnection := func(conn net.Conn, handleLine func(line *Line)) {
-		reader := bufio.NewReader(conn)
-		for {
-			rawline, err := reader.ReadString('\n')
-			if err == io.EOF {
-				return
-			}
-
-			if err != nil {
-				log.Printf("reading from connection failed: %s\n", err.Error())
-				return
-			}
-
-			line, err := ParseLine(rawline)
-			if err != nil {
-				log.Printf("parsing line failed: %s\n", err.Error())
-				return
-			}
-
-			handleLine(line)
-		}
 	}
 
 	go func() {
@@ -185,57 +83,37 @@ func ReceiveTCP(address string, handleLine func(line *Line), done chan bool) err
 			return err
 		}
 
-		go handleConnection(conn, handleLine)
+		go func() {
+			reader := bufio.NewReader(conn)
+			dec := lineprotocol.NewDecoder(reader)
+			handleLine(dec)
+		}()
 	}
 }
 
 // Connect to a nats server and subscribe to "updates". This is a blocking
 // function. handleLine will be called for each line recieved via nats.
 // Send `true` through the done channel for gracefull termination.
-func ReceiveNats(address string, handleLine func(line *Line), workers int, ctx context.Context) error {
+func ReceiveNats(address string, handleLine func(dec *lineprotocol.Decoder), workers int, ctx context.Context) error {
 	nc, err := nats.Connect(nats.DefaultURL)
 	if err != nil {
 		return err
 	}
 	defer nc.Close()
 
+	var wg sync.WaitGroup
 	var sub *nats.Subscription
 
-	if workers < 2 {
-		sub, err = nc.Subscribe("updates", func(m *nats.Msg) {
-			lines, err := ParseLines(string(m.Data))
-			if err != nil {
-				log.Println(err.Error())
-			}
+	msgs := make(chan *nats.Msg, workers*2)
 
-			for _, line := range lines {
-				handleLine(line)
-			}
-		})
-		if err != nil {
-			return err
-		}
-
-		log.Printf("NATS subscription to 'updates' on '%s' established\n", address)
-
-		<-ctx.Done()
-		err = sub.Unsubscribe()
-	} else {
-		msgs := make(chan *nats.Msg, 16)
-		var wg sync.WaitGroup
+	if workers > 1 {
 		wg.Add(workers)
 
 		for i := 0; i < workers; i++ {
 			go func() {
 				for m := range msgs {
-					lines, err := ParseLines(string(m.Data))
-					if err != nil {
-						log.Println(err.Error())
-					}
-
-					for _, line := range lines {
-						handleLine(line)
-					}
+					dec := lineprotocol.NewDecoderWithBytes(m.Data)
+					handleLine(dec)
 				}
 
 				wg.Done()
@@ -245,17 +123,23 @@ func ReceiveNats(address string, handleLine func(line *Line), workers int, ctx c
 		sub, err = nc.Subscribe("updates", func(m *nats.Msg) {
 			msgs <- m
 		})
-		if err != nil {
-			return err
-		}
-
-		log.Printf("NATS subscription to 'updates' on '%s' established\n", address)
-
-		<-ctx.Done()
-		err = sub.Unsubscribe()
-		close(msgs)
-		wg.Wait()
+	} else {
+		sub, err = nc.Subscribe("updates", func(m *nats.Msg) {
+			dec := lineprotocol.NewDecoderWithBytes(m.Data)
+			handleLine(dec)
+		})
 	}
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("NATS subscription to 'updates' on '%s' established\n", address)
+
+	<-ctx.Done()
+	err = sub.Unsubscribe()
+	close(msgs)
+	wg.Wait()
 
 	if err != nil {
 		return err
